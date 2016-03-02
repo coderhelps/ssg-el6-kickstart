86b680a6c1d30e57ba72f914a3590d90ece2e79b
coderhelps <tim@coderhelps.com>
2016-03-02 15:10:30 -0500

    added support for centos

--- createiso.sh
+++ createiso.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+#set -x # debug flag
 ###############################################################################
 # HARDENED RHEL DVD CREATOR
 #
@@ -22,9 +23,9 @@ function usage() {
 cat << EOF
 usage: $0 rhel-server-6.5-x86_64-dvd.iso
 
-Hardened RHEL Kickstart RHEL 6.4+
+Hardened RHEL/CENTOS Kickstart for version 6.4+
 
-Customizes a RHEL 6.4+ x86_64 Server or Workstation DVD to install
+Customizes a RHEL/CENTOS 6.4+ x86_64 DVD to install
 with the following hardening:
 
   - DISA STIG/USGCB/NSA SNAC for Red Hat Enterprise Linux
@@ -72,65 +73,97 @@ fi
 # Determine if DVD is Bootable
 `file $1 | grep 9660 | grep -q bootable`
 if [[ $? -eq 0 ]]; then
-	echo "Mounting RHEL DVD Image..."
-	mkdir -p /rhel
-	mkdir $DIR/rhel-dvd
-	mount -o loop $1 /rhel
+	echo "Mounting DISTRO DVD Image..."
+	mkdir -p /distro
+	mkdir $DIR/distro-dvd
+	mount -o loop $1 /distro
 	echo "Done."
-	
-	# Determine RHEL Version
-	if [ -e /rhel/.discinfo ]; then
-		RHEL_VERSION=$(grep "Red Hat" /rhel/.discinfo | awk '{ print $5 }')
-		MAJOR=$(echo $RHEL_VERSION | awk -F '.' '{ print $1 }')
-		MINOR=$(echo $RHEL_VERSION | awk -F '.' '{ print $2 }')
-		if [[ $MAJOR -ne 6 ]]; then
+
+    # Support RHEL & CentOS
+    if [[ -e /distro/.discinfo ]]; then
+        # check to see if centos or rhel
+
+        RHEL_VERSION=$(grep "Red Hat" /distro/.discinfo | awk '{ print $5 }')
+        CENTOS_VERSION=$(grep "^[0-9]\{1\}\.[0-9]\{1\}$" /distro/.discinfo)
+
+        # if rhel contains version number
+        if [[ -n "$RHEL_VERSION" ]]; then
+            DISTRO="rhel"
+        elif [[ -n "$CENTOS_VERSION" ]]; then
+            DISTRO="centos"
+        else
+		    echo "ERROR: Image is not RHEL....exiting"
+		    exit 1
+        fi
+        
+        if [[ "$DISTRO" -eq "centos" ]]; then
+            DISTRO_VERSION=$CENTOS_VERSION
+        elif [[ "$DISTRO" -eq "rhel" ]]; then
+            DISTRO_VERSION=$RHEL_VERSION
+        else
+            echo "ERROR: Distro name not being set properly....exiting"
+            exit 1
+        fi
+
+        # verify proper version number
+        MAJOR_VERSION=$(echo $DISTRO_VERSION | awk -F '.' '{ print $1 }')
+        MINOR_VERSION=$(echo $DISTRO_VERSION | awk -F '.' '{ print $2 }')
+
+        # verify that we are using a "6" version
+        if [[ $MAJOR_VERSION -ne 6 ]]; then
 			echo "ERROR: Image is not RHEL 6.4+"
-			umount /rhel
-			rm -rf /rhel
+			umount /distro
+			rm -rf /distro
+			exit 1
+        fi
+
+        # verify that we are using a "4+" version
+		if [[ $MINOR_VERSION -lt 4 ]]; then
+			echo "ERROR: Image is not RHEL 6.4+"
+			umount /distro
+			rm -rf /distro
 			exit 1
 		fi
-		if [[ $MINOR -lt 4 ]]; then
-			echo "ERROR: Image is not RHEL 6.4+"
-			umount /rhel
-			rm -rf /rhel
-			exit 1
-		fi
-	else
-		echo "ERROR: Image is not RHEL"
-		exit 1
-	fi
 
+	    echo -n "Copying DISTRO DVD Image..."
+	    cp -a /distro/* $DIR/distro-dvd/
+	    cp -a /distro/.discinfo $DIR/distro-dvd/
+	    echo " Done."
+	    umount /distro
+	    rm -rf /distro
 
-	echo -n "Copying RHEL DVD Image..."
-	cp -a /rhel/* $DIR/rhel-dvd/
-	cp -a /rhel/.discinfo $DIR/rhel-dvd/
-	echo " Done."
-	umount /rhel
-	rm -rf /rhel
+        echo -n "Modifying DISTRO DVD Image..."
+        sed -i "s/6.X/$DISTRO_VERSION/g" $DIR/config/isolinux/isolinux.cfg
+        sed -i "s/DISTRO NAME/$DISTRO_NAME/g" $DIR/config/isolinux/isolinux.cfg
+        cp -a $DIR/config/* $DIR/distro-dvd/
+        sed -i "s/$DISTRO_VERSION/6.X/g" $DIR/config/isolinux/isolinux.cfg
+        sed -i "s/$DISTRO_NAME/DISTRO NAME/g" $DIR/config/isolinux/isolinux.cfg
+        echo " Done."
+
+        echo "Remastering DISTRO DVD Image..."
+        cd $DIR/distro-dvd
+        chmod u+w isolinux/isolinux.bin
+        find . -name TRANS.TBL -exec rm '{}' \; 
+        /usr/bin/mkisofs -J -T -o $DIR/ssg-$DISTRO-$DISTRO_VERSION.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -m TRANS.TBL .
+        cd $DIR
+        rm -rf $DIR/distro-dvd
+        echo "Done."
+
+        # cleanup
+        rm config/isolinux/sed*
+
+        # signing dvd
+        echo "Signing $DISTRO DVD Image..."
+        /usr/bin/implantisomd5 $DIR/ssg-$DISTRO-$DISTRO_VERSION.iso
+        echo "Done."
+
+        echo "DVD Created. [ssg-$DISTRO-$DISTRO_VERSION.iso]"
+
+    fi
+
 else
 	echo "ERROR: ISO image is not bootable."
 	exit 1
 fi
-
-echo -n "Modifying RHEL DVD Image..."
-sed -i "s/6.X/$RHEL_VERSION/g" $DIR/config/isolinux/isolinux.cfg
-cp -a $DIR/config/* $DIR/rhel-dvd/
-sed -i "s/$RHEL_VERSION/6.X/g" $DIR/config/isolinux/isolinux.cfg
-echo " Done."
-
-echo "Remastering RHEL DVD Image..."
-cd $DIR/rhel-dvd
-chmod u+w isolinux/isolinux.bin
-find . -name TRANS.TBL -exec rm '{}' \; 
-/usr/bin/mkisofs -J -T -o $DIR/ssg-rhel-$RHEL_VERSION.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -R -m TRANS.TBL .
-cd $DIR
-rm -rf $DIR/rhel-dvd
-echo "Done."
-
-echo "Signing RHEL DVD Image..."
-/usr/bin/implantisomd5 $DIR/ssg-rhel-$RHEL_VERSION.iso
-echo "Done."
-
-echo "DVD Created. [ssg-rhel-$RHEL_VERSION.iso]"
 
 exit 0
